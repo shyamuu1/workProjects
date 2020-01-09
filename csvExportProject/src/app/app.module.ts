13a8f7abe2a1b449e6b8d5807b17e9ce8cd16839
shyamuu1 <shyamuu.vasanjee@gmail.com>
2020-01-08 23:16:23 -0500

    added Angular Material and ReactiveForms

--- csvExportProject/src/app/app.module.ts
+++ csvExportProject/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MatFormFieldModule, MatInputModule, MatLabel, MatOptionModule, MatSelectModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +15,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- csvExportProject/src/app/data-form/data-form.component.ts
+++ csvExportProject/src/app/data-form/data-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Data } from '../data';
 import { ExportService } from '../export.service';
 import { UserService } from '../user.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -13,26 +14,30 @@ export class DataFormComponent implements OnInit {
   titles = ["Developer", "Data Analytics", "Sales"];
   model: Data;
   submitted = false;
-  objectMap = new Map();
 
   constructor(private es: ExportService, private us: UserService) { }
+  dataForm =  new FormGroup({
+    fullname: new FormControl('',Validators.required),
+    initials: new FormControl('', Validators.required),
+    jobTitle: new FormControl('', Validators.required)
+
+  })
 
   ngOnInit() {
     this.us.findAll().subscribe(data => {
       this.users = data;
     });
-    //this.model = this.dataArray[0];
   }
-  
-  onSubmit(value) {
+
+  onSubmit() {
     this.submitted = true;
-    this.addData(value);
+    this.addData(this.dataForm.value);
   }
 
   addData(value) {
     let id: number = this.users.length +1;
     let valid: boolean = false;
-    let newData: Data = new Data(id, value.fullname, value.initials.toUpperCase(), value.title);
+    let newData: Data = new Data(id, value.fullname, value.initials.toUpperCase(), value.jobTitle);
     this.users.forEach(user => {
       if (newData.initials == user.initials) {
         valid = true;
